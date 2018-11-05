bad173d349d62dc706e797dcbf536cf8e7673547
Emily Green <green253@purdue.edu>
2018-11-05 11:36:55 -0500

    Update insertData.php

--- insertData.php
+++ insertData.php
@@ -5,27 +5,38 @@
 </head>
 <body>
 <?php
-$warehouse_name=$_GET["warehouse_name"];
-$location=$_GET["location"];
-$size_storage=$_GET["size_storage"];
-$conditions=$_GET["conditions"];
-$year_built=$_GET["year_built"];
-$start_date=$_GET["start_date"];
-$end_date=$_GET["end_date"];
-$price=$_GET["price"];
-$security=$_GET["security"];
-$storage=$_GET["storage"];
+$warehouse_name=$_POST['warehouse_name'];
+$location=$_POST['location'];
+$size_storage=$_POST['size_storage'];
+$conditions=$_POST['conditions'];
+$year_built=$_POST['year_built'];
+$start_date=$_POST['start_date'];
+$end_date=$_POST['end_date'];
+$price=$_POST['price'];
+$security=$_POST['security'];
+$storage=$_POST['storage'];
 
-$conn = mysql_connect("mydb.ics.purdue.edu","green253","Classof2015");
-mysql_select_db("WareReg");
-mysql_query("insert into WareReg values('$warehouse_name','$location', '$size_storage', '$conditions', '$year_built','$start_date','$end_date','$price','$security','$storage')");
+define('DB_NAME', 'green253');
+define('DB_USER', 'green253');
+define('DB_PASSWORD', 'Classof2015');
+define('DB_HOST', 'mydb.ics.purdue.edu');
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
+$link = mysql_connect(DB_HOST,DB_USER,DB_PASSWORD);
 
+if(!link){
+	die('Could not connect: ' . mysql_error());
+}
+$db_selected = mysql_select_db(DB_NAME, $link);
+if (!$db_selected){
+	die('Can\'t use ' .DB_NAME . ':'.mysql_error());
+}
+
+
+$sql = "INSERT INTO WareReg values('$warehouse_name','$location', '$size_storage', '$conditions', '$year_built','$start_date','$end_date','$price','$security','$storage')");
+if(!mysql_query($sql)){
+	die('Error: ' .mysql_error());
+}
+mysql_close();
 ?>
 
 </body>
