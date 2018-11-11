f05f5ff41e942eaee1164553d310484fa81055e5
Emily Green <green253@purdue.edu>
2018-11-11 17:32:26 -0500

    Update contractText.php

---
+++ contractText.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p>This Lease, made and executed on ________ by and between ___________(warehouse owner), hereinafter called Landlord, and ______________, hereinafter called Tenant.</p>
+
+<h3>WITNESSETH:</h3>
+<p>1. DESCRIPTION OF PREMISES. The Landlord does hereby lease to Tenant the premises located at: ____ in ___________;</p>
+<br>
+<p>2. TERM. The term of this Lease shall commence on ______________ and shall expire on _________________.
+If Tenant shall remain in possession of all or any part of the Leased Premises after the expiration of the term of this Lease, with the consent of the Landlord, 
+then the Tenant shall be a Tenant from month to month at the same rental and subject to all of the other applicable covenants, terms and conditions hereof, 
+and Tenant shall be required to give thirty (30) days advance notice before vacating the premises.</p>
+<br>
+<p>3. RENT. The Tenant promises to pay to the Landlord as rent the sum of __________ for the whole term of the Lease, payable at the time of the making of this Lease. 
+For the convenience of the Tenant only, said rent may be paid in equal monthly installments at the rate of __________ each, in advance on the first day of each and every month, 
+during the entire term hereof. Rent for the first month will be prorated if applicable. In the event of any default in the payment of the rent in any of the monthly 
+installments as herein provided, then all of the remaining rent for the entire lease term shall, at once, become due and be payable. 
+All payment shall be due without any notice or demand, from the Landlord.</p>
+<br>
+<p>4. ASSIGNMENT AND OCCUPANCY. Tenant shall not assign this Lease, or any interest under it, or sublet the premises or any part thereof during the term of this Lease. 
+Tenant covenants and agrees that, unless an additional child be born to the Tenant during tenancy no one other than the Tenant and the following people shall occupy the leased premises.</p>
+<br>
+<p>5. USE OF PREMISES. Tenant shall occupy and use the premises for ______________, and for no other purpose. Tenant shall keep the premises in a clean and 
+sanitary condition and shall not make or permit any use of which, directly or indirectly, is forbidden by law, ordinance, or governmental regulation, 
+which is dangerous to life, limb or property, which will or may tend to injure the reputation of the premises or which may invalidate or increase the premium 
+cost of any policy of insurance carried by Landlord in connection with its operation and management of the building.</p>
+<br>
+<p>6. MAINTENANCE. Tenant warrants that he/she examined the premises, and agrees that insofar as he/she can determine, the premises, including interior 
+and exterior of building, and all appliances furnished, are in GOOD and CLEAN and SATISFACTORY CONDITION. Tenant shall continuously maintain at his/her own 
+expense, the leased premises during the term of this Lease in as good and clean and satisfactory a condition as when the Tenant took possession. Furthermore,
+ at the termination of this Lease, the premises shall be delivered to the Landlord in said good and clean and satisfactory condition, and upon default of this condition, 
+ the Landlord may as the Tenant’s agent, restore said premises to such condition and obligate the Tenant therefore, natural wear and tear and Acts of God excepted. 
+ Natural wear is that wear which would foreseeably result from a careful use of property as if it were one’s own and with due regard to its preservation and good order. </p>
+<br>
+<p>7. UTILITIES. Tenant shall pay for utilities to include, cable, electricity, gas, and telephone use charges, and shall transfer said utilities to tenant’s 
+name before occupying the leased premises.</p>
+<br>
+<p>8. RESERVED RIGHTS. Landlord reserves the following rights to wit: To enter premises, or any part thereof, at all reasonable hours for inspection, 
+repairs, alterations or additions; to exhibit the premises to prospective Tenants, purchasers or others; to display without molestation by the Tenant “For Rent” 
+and similar signs; to enter the premises for any purpose whatsoever related to the safety, protection, preservation or improvement of the premises; and retain and 
+use passkeys to the premises. The exercise of reserved rights of the Landlord shall never be deemed an eviction or disturbance of Tenant’s peaceable possession of 
+the premises and shall never render Landlord liable in any manner to Tenant or to any person in the premises. </p>
+<br>
+<p>9. LANDLORDS NON LIABILITY. Landlord shall not be liable for damages to person or property sustained by the Tenant, his family, servants, agents or visitors, 
+due to the building or any appurtenances becoming out of repair or arising from leakage of gas, steam, water or sewer pipes, or from defective wiring, or water 
+damage of any kind or nature. Tenant shall indemnify and hold Landlord harmless for any claim or damages to personal property or personal injury arising out of 
+Tenant’s use or occupancy of the leased premises, including any claim for injury to Tenant’s guests and invitees. Tenant has been advised to secure insurance for 
+his own possessions and liabilities. No act or thing done by the Landlord or Landlord’s agents or employees during the leased term shall constitute either an eviction 
+by the Landlord or an acceptance, an agreement to accept the surrender of said premises, unless the same is evidence by an instrument in writing which is signed 
+by the Landlord. The acceptance of delivery of the keys for the Leased Premises by the Landlord prior to the date of Lease termination as provided for in 
+paragraph #2 shall not act as a termination of this Lease.</p>
+<br>
+<p>10. FIRE AND CASUALTY. Tenant shall solely be responsible for checking and maintaining the smoke detector on the Leased premises, including testing the smoke 
+detector periodically and maintaining a working battery in same. In the event the Leased Premises are substantially damaged by fire, wind, explosion or other 
+cause beyond the Landlord, or if the premises or access thereto are condemned or are acquired by properly constituted governmental or other authority the Landlord 
+may, at its option, terminate this Lease. </p>
+<br>
+<p>11. NOTICES AND DEMANDS. All notices and demands authorized or required to be given the Tenant hereunder, except notice of termination as provided for in 
+paragraph 2, may be served upon Tenant in person or regular mail and addressed to him at the Leased Premises. All notices required to be given hereunder to 
+the Landlord, and all rent payments, shall be sent Landlord at the address listed on the last page of this Lease. </p>
+<br>
+<p>12. DUTY OF TENANT UPON TERMINATION OF LEASE. Upon the expiration or termination of this Lease, shall immediately vacate the premises, removing all of 
+his/her personal property, including any trash or rubbish. Any of the personal property of the Tenant remaining on the leased property after the expiration 
+or termination of this Lease for non-payment of rent or for any other reason shall be deemed abandoned property and Tenant hereby authorizes Landlord to discard 
+said property or dispose of it in any manner which Landlord deems appropriate, and Tenant hereby agrees to pay to Landlord any cost associated with the 
+disposition of said property and Tenant further waives all claims which he may have against Landlord for disposing of said personal property. </p>
+<br>
+<p>13. REMOVAL OF PERSONAL PROPERTY. If after violation of any provision of this Lease, or upon the expiration of this Lease, if Tenant moves out and fails 
+to removal all of his or her personal property, then the remaining personal property shall be deemed to be abandoned. The Landlord shall have the right to 
+dispose of said personal property in any manner, and Tenant waives any right or claim whatsoever to said property, including any claim for loss or damage 
+to any personal property remaining on the leased premises.</p>
+<br>
+<p>14. RIGHTS OF LANDLORD UPON DEFAULT. Upon failure to pay any installment of rent or any part thereof when due, or if the Tenant shall violate any other 
+term, condition or covenant of this Lease, or if the Tenant shall fail promptly to take possession of or shall abandon the premises, Landlord shall have the 
+right to re-enter and repossess the premises, to remove all persons therefrom and to remove all property therefrom and in each event this Lease and all rights 
+of the Tenant as Tenants shall terminate, but the Tenant shall remain liable for the rent herein specified during the remaining term of this Lease plus Landlord’s 
+cost of repossessing the premises. Tenant hereby waives notice of any failure or default and of any demand by Landlord for possession of the premises. In the event 
+Landlord shall repossess the premises, Landlord shall not be required to exercise diligence in re-letting the premises in order to mitigate Tenant’s obligations hereunder,
+nor will it be required to accept any Tenant for the premises offered by Tenant. The failure on the part of the Landlord to re-enter or repossess the premises, 
+or to exercise any of its rights hereunder, upon default, shall not preclude the Landlord from the exercise of such rights during the continuance of such default 
+or upon any subsequent default. Acceptance of past due rent will in no way act as a waiver of Landlord’s right to terminate the Lease for non-payment of rent when due, 
+and no notice or demand shall be required for the enforcement thereof. If Tenant shall violate or breach the terms of this Lease, the Tenant shall pay all cost and expenses, 
+including attorneys fees, incurred by Landlord in connection with exercising any rights or remedies it may have under this Lease because of such violation or breach. </p>
+<br>
+<p>15. SUBORDINATION. This Lease will be subject and subordinate at all time to the lien of any existing mortgages and of mortgages which may hereafter be made a lien 
+on the premises. Tenant shall execute any and all documents reasonably required by Landlord’s mortgage lender to subordinate the Lease to the mortgage. </p>
+<br>
+<p>16. CONDITIONS. This Lease is made upon the following conditions, each of which Tenant covenants will be kept, 
+observed, and performed, throughout the term of this Lease. </p>
+
+<br>
+<br>
+
+<p>(A) Tenant will take care of the premises and fixtures and will make good any injury thereto done by him/her, and will reimburse Landlord for any damage caused 
+by the escape or overflow of water resulting from his/her negligence. </p>
+<br>
+<p>(B) Tenant will give Landlord or its agents prompt notice of any defects or breakage in the structure, equipment, or fixtures of said premises. 
+Tenant shall also give Landlord immediate notice of any water leaks of any kind or nature. Notice to the Landlord may be given at the following numbers:
+ 	__________________________________________________________ </p>
+<br>
+<p>(C) Tenant will at termination and prior to surrender of possession have the premises thoroughly cleaned and in the event of failure so to clean, 
+he/she will pay Landlord cost of having same done.</p>
+<br>
+<p>(D) Tenant will be responsible for rental insurance to cover his/her personal property and liability.</p>
+<br>
+<p>(E) Tenant shall be responsible for all snow and ice removal.</p>
+<br>
+<br>
+<p>17. ENTIRE AGREEMENT. This Lease contains the entire agreement between the parties, and Tenant agrees that neither Landlord, or any agent or 
+representative of the Landlord, has made any statement, promise, or agreement which is not expressly set forth within this Lease.</p>
+<br>
+<p>18. RENTAL APPLICATION. The parties further agree that any misrepresentation on the rental application submitted prior to 
+entering into this Lease shall constitute a default of this Lease.</p>
+<br>
+<br>
+
+<p>____________________________<br>
+TENANT, </p>
+
+<br>
+<br>
+<p>_____________________________<br>
+By: <br>
+LANDLORD</p>
+
+</body>
+</html>
