43728e72616d37266a6b97b8e24eacb5a3e40766
Emily Green <green253@purdue.edu>
2018-11-13 09:57:02 -0500

    Update resultsLoad.php

---
+++ resultsLoad.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<?php
+
+ /*?>$servername = "mydb.itap.purdue.edu";
+$username = "green253";
+$password = "Classof2015";
+$dbname = "green253";<?php */
+
+ /*// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}  */
+	
+ /*$output=array("1","2","3","4","5","6")		
+testing($output);		
+
+function testing($output) {
+	$length = count($output);
+	$i = 1;
+	while ($i < $length) {
+	$output[i] = $id;
+		echo($id);
+	$i = $i + 1;
+		echo($i);
+	}
+} */
+function testing(){
+		echo("works")
+	}
+?>
+
+<body>
+</body>
+</html>
+<!--$sql="SELECT warehouse_name, location, size_storage, conditions, year_built, start_date, end_date, warehouse_price, security, storage FROM WareReg WHERE $id=warehouse_id";
+$id = intval($_GET['id']);-->
